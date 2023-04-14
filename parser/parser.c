6cb50933c60117da1c3beae1b761135b8d6dbb42
Borbold <ivan0.111@mail.ru>
2023-04-14 12:11:25 +0700

    Delete fscan. Change in fread.

--- parser/parser.c
+++ parser/parser.c
@@ -13,6 +13,8 @@ static void read_param(FILE *data_file);
 static void read_variable(FILE *data_file);
 static void read_constant(FILE *data_file);
 
+static void make_blank(char *name);
+
 char *fill_out(char r_b, char *buff, int *r_count) {
   if (r_b == ' ' || r_b == '\n') {
     buff = malloc(64);
@@ -20,6 +22,18 @@ char *fill_out(char r_b, char *buff, int *r_count) {
   } else {
     buff[*r_count] = r_b;
     *r_count += 1;
+  }
+  return buff;
+}
+
+char *get_buff(FILE *data_file) {
+  int r_count = 0;
+  char *buff = malloc(64);
+  char r_b;
+  while (fread(&r_b, 1, 1, data_file) > 0) {
+    if (r_b == '\n')
+      break;
+    fill_out(r_b, buff, &r_count);
   }
   return buff;
 }
@@ -44,66 +58,42 @@ void parse_file() {
           read_variable(data_file);
         } else if (strcmp(buff, basic_command[START_TIME]) == 0) {
           printf("%sStart time exist%s\n", "\033[1;33m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[CONSTANT]) == 0) {
           read_constant(data_file);
         } else if (strcmp(buff, basic_command[LATITUDE]) == 0) {
           printf("%sLATITUDE exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[LONGITUDE]) == 0) {
           printf("%sLONGITUDE exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[STEPS]) == 0) {
           printf("%sSTEPS exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[WAIT_COORDINATES]) == 0) {
           printf("%sWAIT_COORDINATES exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[ORIENTATION]) == 0) {
           printf("%sORIENTATION exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[PAYLOAD_POWER]) == 0) {
           printf("%sPAYLOAD_POWER exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[WAIT]) == 0) {
           printf("%sWait exist%s\n", "\033[1;35m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[CREATE_METER]) == 0) {
           printf("%sCREATE_METER exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[ADD_CHANAL_METER]) == 0) {
           printf("%sADD_CHANAL_METER exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[RUN_METER]) == 0) {
           printf("%sRUN_METER exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[STOP_METER]) == 0) {
           printf("%sSTOP_METER exist%s\n", "\033[1;34m", "\033[0m");
-          char var[200];
-          fscanf((FILE *)data_file, "%s", var);
-          printf("%s\n", var);
+          make_blank(get_buff(data_file));
         } else if (strcmp(buff, basic_command[EXIT]) == 0) {
           printf("%sEXIT exist%s\n", "\033[1;34m", "\033[0m");
         }
@@ -127,31 +117,20 @@ void make_variable(char *name, char *type) {
 void make_constant(char *name, char *type) {
   printf("Create constant %s with type %s\n", name, type);
 }
+void make_blank(char *name) { printf("%s\n", name); }
 /* Make */
 
 /* Read */
 void read_param(FILE *data_file) {
   printf("%sParams exist%s\n", "\033[1;34m", "\033[0m");
-  int r_count = 0;
-  char *buff = malloc(64);
-  char r_b;
-  while (fread(&r_b, 1, 1, data_file) > 0) {
-    if (r_b == '\n')
-      break;
-    fill_out(r_b, buff, &r_count);
-  }
-  make_param(buff, "TYPE");
+  make_param(get_buff(data_file), "TYPE");
 }
 void read_variable(FILE *data_file) {
   printf("%sVariables exist%s\n", "\033[1;34m", "\033[0m");
-  char var[200];
-  fscanf((FILE *)data_file, "%s", var);
-  printf("%s\n", var);
+  make_param(get_buff(data_file), "TYPE");
 }
 void read_constant(FILE *data_file) {
   printf("%sConstant exist%s\n", "\033[1;34m", "\033[0m");
-  char var[200];
-  fscanf((FILE *)data_file, "%s", var);
-  printf("%s\n", var);
+  make_param(get_buff(data_file), "TYPE");
 }
 /* Read */
