49fa7796421431554eba6e82b1967bb16182706f
Borbold <ivan0.111@mail.ru>
2023-04-20 15:19:51 +0700

    Small fix

--- parser/s_parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-typedef enum { PROGRAM, VARIABLES, CONSTANT, STEPS } basic_string;
-
-const char *const basic_command[] = {"program", "variables", "constant",
-                                     "steps", 0};
-
-#endif
